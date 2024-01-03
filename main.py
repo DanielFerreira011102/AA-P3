b56397d9a5f4a156f08ce9ac995b0919322bae41
leina <djbf@ua.pt>
2024-01-03 19:39:35 +0000

    update

--- main.py
+++ main.py
@@ -4,51 +4,63 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
 
 sns.set_style("whitegrid")
 
 
 def main():
     work = 'republic'
-    ex_files = {
-        'en': f'results\{work}\en\exact.csv',
-        'pt': f'results\{work}\pt\exact.csv',
-    }
     files = {
-        'en': {'cms': f'results\{work}\en\cms.csv'},
-        'pt': {'cms': f'results\{work}\pt\cms.csv'},
+        'en': {'fixed': f'results\{work}\en\\fixed.csv'},
+        'pt': {'fixed': f'results\{work}\pt\\fixed.csv'},
     }
 
-    ex_en_df = pd.read_csv(ex_files['en'])
-    ex_pt_df = pd.read_csv(ex_files['pt'])
+    p = {'en': '#000', 'pt': '#aaa'}
 
-    ex_en_br = ex_en_df['BR'][0]
-    ex_pt_br = ex_pt_df['BR'][0]
+    en_df = pd.read_csv(files['en']['fixed'])
+    pt_df = pd.read_csv(files['pt']['fixed'])
 
-    for lang in files:
-        for method in files[lang]:
-            filename = files[lang][method]
-            df = pd.read_csv(filename)
+    p_values = en_df['probability']
+    en_total_bits_saved = en_df['BS']
+    pt_total_bits_saved = pt_df['BS']
+    en_mean_relative_error = en_df['mean_relative_error']
+    pt_mean_relative_error = pt_df['mean_relative_error']
 
-            df['example_sketch'] = df['example_sketch'].apply(eval)
-            df['example_sketch_map'] = df['example_sketch_map'].apply(eval)
-            df['counter_transformed'] = df['counter_transformed'].apply(eval)
+    scaler = MinMaxScaler()
+    en_total_bits_saved_normalized = scaler.fit_transform(en_total_bits_saved.values.reshape(-1, 1))
+    pt_total_bits_saved_normalized = scaler.fit_transform(pt_total_bits_saved.values.reshape(-1, 1))
+    en_mean_relative_error_normalized = scaler.fit_transform(en_mean_relative_error.values.reshape(-1, 1))
+    pt_mean_relative_error_normalized = scaler.fit_transform(pt_mean_relative_error.values.reshape(-1, 1))
 
-            columns_to_drop = ['TBR', 'TBR_transformed', 'PBS', 'PBS_transformed']
+    en_composite_score = (
+        en_total_bits_saved_normalized - en_mean_relative_error_normalized
+    )
+    pt_composite_score = (
+        pt_total_bits_saved_normalized - pt_mean_relative_error_normalized
+    )
+    
+    # Plot the results
+    plt.figure(figsize=(12, 8))
+    # color with s-
+    plt.plot(p_values, en_composite_score, label='English', color=p['en'], marker='s')
+    plt.plot(p_values, pt_composite_score, label='Portuguese', color=p['pt'], marker='s')#
+    # line for best p for each language
+# Vertical lines for the best ρ for each language
+    best_rho_en = p_values[np.argmax(en_composite_score)]
+    best_rho_pt = p_values[np.argmax(pt_composite_score)]
 
-            df = df.drop(columns=columns_to_drop)
+    plt.axvline(x=best_rho_en, color='blue', linestyle='--', label=r'Best $\rho$ (English):' + f'{best_rho_en:.2f}')
+    plt.axvline(x=best_rho_pt, color='red', linestyle='--', label=r'Best $\rho$ (Portuguese):' + f'{best_rho_pt:.2f}')
 
-            df['BR'] = df['example_sketch'].apply(lambda x: sum([value.bit_length() for table in x for value in table]))
-            df['BR_transformed'] = df['counter_transformed'].apply(
-                lambda x: sum([value.bit_length() for value in x.values()]) + sum([len(key) * 8 for key in x.keys()]))
-
-            # BS is (ex_en_br or ex_pt_br depending on the language - df['BR']) / ex_en_br or ex_pt_br depending on the language
-            y = ex_en_br if lang == 'en' else ex_pt_br
-            df['BS'] = df['BR'].apply(lambda x: (y - x) / y)
-            df['BS_transformed'] = df['BR_transformed'].apply(lambda x: (y - x) / y)
-
-            df.to_csv(filename, index=False)
-
+    plt.xlabel(r"$\rho$", fontsize=20)
+    plt.ylabel("BR", fontsize=20)
+    plt.grid(linestyle='--')
+    plt.legend(loc='best', frameon=False, fontsize=20)
+    plt.tick_params(axis="both", which="major", labelsize=18)
+    plt.tight_layout()
+    plt.savefig(f"images/republic/fixed_composite_score.png", bbox_inches='tight', format='png')
+    plt.show()
 
 if __name__ == "__main__":
     main()
